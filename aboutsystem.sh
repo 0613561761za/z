b3a107660c5f7cf146a9f9485d1785ad34cae7c3
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:51:32 +0700

    Update aboutsystem.sh

--- aboutsystem.sh
+++ aboutsystem.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#info
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 
 clear
 neofetch
