9a81571ad429c5dfddb26fa20ddda5df7be90e6d
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 09:17:52 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,18 +5,18 @@ clear
 echo ""
           echo -e "\e[031;1m     
                          
-                =============== OS-32 & 64-bit ================
-                ♦                                             ♦
-                ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
-                ♦       -----------About Us------------       ♦ 
-                ♦            Telp : 097-026-7262              ♦
-                ♦         { VPN / SSH / OCS PANEL }           ♦ 
-                ♦       http://facebook.com/Ceolnw            ♦    
-                ♦             Line id : ceolnw                ♦
-                ♦                                             ♦
-                =============== OS-32 & 64-bit ================
+  =============== OS-32 & 64-bit ================
+  ♦                                             ♦
+  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+  ♦       -----------About Us------------       ♦ 
+  ♦            Telp : 097-026-7262              ♦
+  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+  ♦         http://facebook.com/Ceolnw          ♦    
+  ♦             Line id : ceolnw                ♦
+  ♦                                             ♦
+  =============== OS-32 & 64-bit ================
                 
-                    >>>>> Thank You For Choice Us <<<<<"
+      >>>>> Thank You For Choice Us <<<<<"
 			
 	echo ""
 echo -e "\033[1;31m { a } สร้างรหัส ผู้ใช้งาน openvpn ssh"
