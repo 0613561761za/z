af79f564fcc35bcae9010ce6572ca51871e17e61
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:54:02 +0700

    Update repass.sh

--- repass.sh
+++ repass.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 echo "=================================="
 read -p " Username to edit : " namer
 read -p " formato AAAA/MM/DD : " date
