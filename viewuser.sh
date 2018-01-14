4dde68815f46d2927bf860383cf522bc602ab8cb
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:55:15 +0700

    Update viewuser.sh

--- viewuser.sh
+++ viewuser.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#By AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 echo -e "\033[1;32m
 ====================================================
  credit   : www.เฮียเบิร์ด.com 
