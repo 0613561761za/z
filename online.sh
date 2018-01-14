fc240c1983fb4a2e058c33183a32d68538ca6ee8
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:53:29 +0700

    Update online.sh

--- online.sh
+++ online.sh
@@ -1,4 +1,15 @@
-#AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 
 echo -e ""
 	echo -e "Total Data Usage:"
