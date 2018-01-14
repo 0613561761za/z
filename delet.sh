bf459bef8815c614f91c2ded09bf6c626fe40bd3
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:52:04 +0700

    Update delet.sh

--- delet.sh
+++ delet.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#Script del SSH & OpenVPN
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 read -p "ใส่ชื่อผู้ใช้ SSH & OpenVPN จะลบออก : " User
 
 if getent passwd $User > /dev/null 2>&1; then
