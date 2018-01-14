4271c86c2a292ee6de2f1edd31bf0ecd11e2bef3
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:53:14 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,6 +1,17 @@
 
 #!/bin/bash
-#Menu
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 clear
 echo ""
           echo -e "\e[033;1m     
