a3841cf1662f2d2feea7f6bee97ceb6be3841735
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 09:07:59 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,34 +2,42 @@
 #!/bin/bash
 #Menu
 clear
-echo -e " "
-echo -e "\033[01;32m  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
-echo -e "  {    Wallcom to server Debian7-8    }"
-echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
-echo -e "  { Script mod by ADMIN-SPNET  }"
-echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
-echo -e "  {   prin { menu } Show menu items   }"
-echo -e "  }~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{"
-echo -e " "
-echo -e "\033[1;31m { a } adduser Create SSH & OpenVPN"
-echo -e "\033[1;33m { b } test new  user"
+echo ""
+          echo -e "\e[031;1m     
+                         
+                =============== OS-32 & 64-bit ================
+                ♦                                             ♦
+                ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+                ♦       -----------About Us------------       ♦ 
+                ♦            Telp : 097-026-7262              ♦
+                ♦         { VPN / SSH / OCS PANEL }           ♦ 
+                ♦       http://facebook.com/Ceolnw            ♦    
+                ♦             Line id : ceolnw                ♦
+                ♦                                             ♦
+                =============== OS-32 & 64-bit ================
+                
+                    >>>>> Thank You For Choice Us <<<<<"
+			
+	echo ""
+echo -e "\033[1;31m { a } สร้างรหัส ผู้ใช้งาน openvpn ssh"
+echo -e "\033[1;33m { b } สร้างรหัส ทดลองใช้"
 echo -e "\033[1;34m { c } Change Name"
 echo -e "\033[1;35m { d } Change Password,Change de logins,Change Exp"
-echo -e "\033[1;36m { e } delete user SSH & OpenVPN"
-echo -e "\033[1;32m { f } Auto delete user Expres"
-echo -e "\033[1;33m { g } user view all member SSH & OpenVPN"
+echo -e "\033[1;36m { e } ลบรหัสผู้ใช้งาน openvpn ssh"
+echo -e "\033[1;32m { f } ลบรหัสผู้ใช้งานที่หมดอายุ Auto"
+echo -e "\033[1;33m { g } ดูรหัสผู้ใช้งานทั้งหมด"
 echo -e "\033[1;34m { h } restart service, OpenVPN and SSH"
-echo -e "\033[1;35m { i } speedtest VPS"
-echo -e "\033[1;36m { j } Check bandwidth total data internet"
-echo -e "\033[1;31m { k } view user online"
-echo -e "\033[1;33m { l } about this system"
-echo -e "\033[1;34m { m } ban user"
-echo -e "\033[1;35m { n } unban user"
+echo -e "\033[1;35m { i } ทดสอบสปีดเน็ต VPS"
+echo -e "\033[1;36m { j } ตรวจสอบข้อมูลอินเทอร์เน็ตแบนด์วิธทั้งหมด"
+echo -e "\033[1;31m { k } ดูผู้ใช้ออนไลน์"
+echo -e "\033[1;33m { l } เกี่ยวกับระบบนี้"
+echo -e "\033[1;34m { m } แบน รหัสผู้ใช้งาน"
+echo -e "\033[1;35m { n } ปลดแบน รหัสผู้ใช้งาน"
 echo -e "\033[1;31m { o } Install And Update File HTTP"
 echo -e "\033[1;32m { p } Add And Edit Credit App HTTP"
-echo -e "\033[1;33m { q } about script auto install"
-echo -e "\033[1;34m { r } Setting Reboot Time"
-echo -e "\033[1;34m { s } Backup And Restore User"
-echo -e "\033[1;35m { reboot } reboot VPS"
-echo -e "\033[1;36m { exit } close Putty/Connecbot/JuiceSSH"
+echo -e "\033[1;33m { q } เกี่ยวกับการติดตั้งสคริปต์อัตโนมัติ"
+echo -e "\033[1;34m { r } ตั้งเวลารีบูตเครื่อง Auto"
+echo -e "\033[1;34m { s } สำรอง และ คืนค่า ข้อมูลรหัสผู้ใช้งาน"
+echo -e "\033[1;35m { reboot } รีบูต VPS"
+echo -e "\033[1;36m { exit } ปิด Putty / Connecbot / JuiceSSH"
 echo -e "\033[1;32m"
