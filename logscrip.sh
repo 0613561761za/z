dd14fabcb6e7f289240f57a64c8b984b9fe8df0d
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:52:57 +0700

    Update logscrip.sh

--- logscrip.sh
+++ logscrip.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Script online dropbear, webmin, squid3, openvpn, openssh
-# AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 echo -e "Log OpenVPN User & IP Login"
 cat /etc/openvpn/log.log
