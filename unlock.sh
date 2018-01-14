bae9094deb7fe2a8de83b471673f5b01aea3cd90
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:54:49 +0700

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# Script unlock dropbear, webmin, squid3, openvpn, openssh
-# AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 echo -e "Please in put name user for unlock or unbanned user"
 
 read -p "Username : " Login
