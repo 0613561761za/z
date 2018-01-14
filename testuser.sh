fd43ef81e54f03a60445a40236a179cb96bd4bb8
lnwshop <33618273+lnwshop@users.noreply.github.com>
2018-01-14 10:54:33 +0700

    Update testuser.sh

--- testuser.sh
+++ testuser.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-#Script auto create trial neko SSH
-#this user expire in 1 day
-#Dev By Sakariya
+#
+#  =============== OS-32 & 64-bit ================
+#  ♦                                             ♦
+#  ♦     AUTOSCRIPT CREATED BY เฮียเบิร์ด แงะตลอด   ♦
+#  ♦       -----------About Us------------       ♦ 
+#  ♦            Telp : 097-026-7262              ♦
+#  ♦         { VPN / SSH / OCS PANEL }           ♦ 
+#  ♦        http://facebook.com/Ceolnw           ♦    
+#  ♦             Line id : ceolnw                ♦
+#  ♦                                             ♦
+#  =============== OS-32 & 64-bit ================
+#
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
